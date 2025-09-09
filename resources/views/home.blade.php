505d776c55254c499e745deb466f458073baf4c2
refat <refat@bhml-bettex.com.bd>
2025-09-09 13:03:10 +0600

    update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,29 +50,6 @@
 
     <div class="row">
         <div class="col-md-8 col-sm-12 ">
-            <div class="dashboard_graph">
-
-                <div class="row x_title">
-                    <div class="col-md-6">
-                        <h3>Transition Details</h3>
-                    </div>
-                    <div class="col-md-6">
-                        <div id="reportrange" class="pull-right"
-                            style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-                            <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-                            <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-12 col-sm-12 ">
-                    <div id="chart_plot_01" class="demo-placeholder"></div>
-                </div>
-
-                <div class="clearfix"></div>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-4 ">
             <div class="x_panel tile fixed_height_320 overflow_hidden">
                 <div class="x_title">
                     <h2>Device Usage</h2>
@@ -148,6 +125,9 @@
                     </table>
                 </div>
             </div>
+        </div>
+        <div class="col-md-4 col-sm-4 ">
+
         </div>
     </div>
     <br />
