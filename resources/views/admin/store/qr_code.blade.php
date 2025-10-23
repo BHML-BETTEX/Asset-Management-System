c59e256a23fe268ba1a4db6a21e7fa49a7a083b7
BHML-BETTEX <refat@bhml-bettex.com.bd>
2025-10-23 16:36:19 +0600

    qr update

--- resources/views/admin/store/qr_code.blade.php
+++ resources/views/admin/store/qr_code.blade.php
@@ -12,23 +12,26 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            font-size: 10pt;
+            font-size: 9pt;
+            text-align: center;
         }
 
         .print-button {
-            margin-bottom: 10px;
-            padding: 5px 15px;
+            margin: 10px;
+            padding: 6px 18px;
             background-color: #007bff;
             color: white;
             border: none;
             cursor: pointer;
             border-radius: 4px;
+            font-size: 10pt;
         }
 
         .print-button:hover {
             background-color: #0056b3;
         }
 
+        /* PRINT STYLES */
         @media print {
             @page {
                 size: 1in 1.5in;
@@ -47,57 +50,46 @@
             #print-area {
                 width: 1in;
                 height: 1.5in;
-                margin: auto;
                 display: flex;
-                margin-bottom: 2px;
                 flex-direction: column;
                 justify-content: center;
                 align-items: center;
                 text-align: center;
-                box-sizing: border-box;
-            }
-
-
-            table {
-                width: 100%;
-                height: 100%;
-                border-collapse: collapse;
+                margin: auto;
             }
 
             img {
                 display: block;
-                width: 80px;
-                height: 80px;
-                margin: auto;
+                width: 0.9in;
+                height: 0.9in;
+                object-fit: contain;
+                margin-bottom: 4px;
+            }
+
+            samp {
+                font-size: 8pt;
+                font-weight: bold;
+                display: block;
             }
         }
     </style>
 </head>
 
 <body>
-    <button class="print-button" onclick="printQRCode()">Print QR Code</button>
+    <button class="print-button" onclick="printQRCode()">🖨️ Print QR Code</button>
 
     <div id="print-area">
-        <table>
-            <tr>
-                <td align="center">
-                    <img
-                        src="data:image/png;base64,{{ base64_encode(
-            QrCode::format('png')
-                ->size(180)        // Higher resolution
-                ->margin(0)        // Remove extra padding
-                ->generate('https://asset.bettex.com/public/store/qr_code_view/' . $qrCode->id)
-        ) }}"
-                        alt="QR Code"
-                        style="display: block; width: 75px; height: 75px; margin: auto;">
-                    <samp>{{ $qrCode->asset_tag }}</samp>
-                </td>
-
-            </tr>
-        </table>
+        <img
+            src="data:image/png;base64,{{ base64_encode(
+                QrCode::format('png')
+                    ->size(600)          // High pixel size for print (300 DPI+)
+                    ->margin(0)          // No white borders
+                    ->errorCorrection('H') // Highest error correction for durability
+                    ->generate('https://asset.bettex.com/public/store/qr_code_view/' . $qrCode->id)
+            ) }}"
+            alt="QR Code">
+        <samp>{{ $qrCode->asset_tag }}</samp>
     </div>
-
-
 
     <script>
         function printQRCode() {
@@ -105,5 +97,4 @@
         }
     </script>
 </body>
-
 </html>
