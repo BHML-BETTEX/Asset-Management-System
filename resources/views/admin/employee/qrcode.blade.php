cb498954570fdf3e66240cfa78753378dbd89570
BHML-BETTEX <refat@bhml-bettex.com.bd>
2025-11-12 15:11:15 +0600

    update

--- resources/views/admin/employee/qrcode.blade.php
+++ resources/views/admin/employee/qrcode.blade.php
@@ -14,9 +14,9 @@
             <table style="width: 350px; font-size: 10pt; font-family: Arial, sans-serif;" cellpadding="0" cellspacing="0">
               <tbody>    
               <tr>
-                <td style="font-size: 12pt; font-family: Arial, sans-serif; padding-left: 10px;"> <span style="text-transform:uppercase; font-size: 12pt; font-family: Arial Black; color:#06A5CC;"> {{name}}</span><br>
-                      <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#5D6D7E;"><strong> {{designation}} || {{department}}<br>
-                      <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#5D6D7E;"><strong>{{$qrCode->phone}}<br>
+                <td style="font-size: 12pt; font-family: Arial, sans-serif; padding-left: 10px;"> <span style="text-transform:uppercase; font-size: 12pt; font-family: Arial Black; color:#06A5CC;"> </span><br>
+                      <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#5D6D7E;"><strong> <br>
+                      <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#5D6D7E;"><strong><br>
                       <span style="font-size: 10pt; font-family: Arial, sans-serif; color:#16b0e5"><strong> <a href="https://bettex.com.bd/">BHML Industries Ltd.</a></strong><br></span></strong></span></td>
               </tr>
              <!-- <tr>
