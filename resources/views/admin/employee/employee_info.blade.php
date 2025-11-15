99d225afac1f72f4f35f4ebf67343fa69746b52a
BHML-BETTEX <refat@bhml-bettex.com.bd>
2025-11-15 23:25:44 +0600

    update

--- resources/views/admin/employee/employee_info.blade.php
+++ resources/views/admin/employee/employee_info.blade.php
@@ -487,14 +487,9 @@
                     <i class="fa fa-envelope"></i> Email Asset List
                 </button>
 
-                <a href="{{ route('employee_view', $employee->id) }}" class="action-btn btn-edit">
-                    <i class="fa fa-edit"></i> QR View
+                <a href="{{ route('employee.qrcode', $employee->emp_id) }}" class="action-btn btn-edit">
+                    <i class="fa fa-edit"></i> QR Signature
                 </a>
-
-                <a href="{{ route('employee.qrcode', $employee->emp_id) }}" class="btn btn-primary text-white">
-                    <i class="fa fa-file-pdf-o"></i> Generate Mail Signature
-                </a>
-
 
                 <div class="dropdown-divider my-3"></div>
 
